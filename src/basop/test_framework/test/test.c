eb3142155c4a21619a58e7586953c390626ded93
Ludovic Malfait <ludovic.malfait@dolby.com>
2018-06-19 17:09:04 -0700

    Returns non 0 if an error occurred.

--- src/basop/test_framework/test/test.c
+++ src/basop/test_framework/test/test.c
@@ -8,37 +8,37 @@ int test_sanity();
 
 int  main()
 {
-    //
     int RetVal = 0;
 
 #ifdef CHECK_SANITY
     printf("Starting Sanity Tests...\n");
-    test_sanity();
+    RetVal = test_sanity();
     printf("Sanity tests Completed\n");
 
-#else // CHECK_SANITY
-    printf("Starting Precision Analysis...\n");
-
-    printf("Creating default test cases for precision testing...\n");
-    create_default_tests_prec();
-    printf("Completed\n");
-
-    printf("Executing various precision tests...\n");
-    RetVal = test_precision();
-
-    if (!RetVal)
-    {
-        printf(" Succesfully completed :: Test result = %d \n", RetVal);
-    }
-    else
-    {
-        printf(" Tests Failure :: Test result = %d \n", RetVal);
-    }
-
-    printf("Tests complete \n");
 #endif // CHECK_SANITY
 
- 	return 0 ;
+    if (!RetVal) {
+      printf("Starting Precision Analysis...\n");
+
+      printf("Creating default test cases for precision testing...\n");
+      create_default_tests_prec();
+      printf("Completed\n");
+
+      printf("Executing various precision tests...\n");
+      RetVal = test_precision();
+
+      if (!RetVal)
+      {
+          printf(" Succesfully completed :: Test result = %d \n", RetVal);
+      }
+      else
+      {
+          printf(" Tests Failure :: Test result = %d \n", RetVal);
+      }
+
+      printf("Tests complete \n");
+  }
+ 	return RetVal ;
 }
 /* End of file */
 
