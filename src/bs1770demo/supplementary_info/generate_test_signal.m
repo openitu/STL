2bfefcb8d11dbdfe60e59a32b5e9b7e9149f6484
ErikNorvell-Ericsson <erik.norvell@ericsson.com>
2022-02-04 15:53:10 +0100

    Updated test vectors and moved MATLAB scripts to supplementary_info folder.

---
+++ src/bs1770demo/supplementary_info/generate_test_signal.m
@@ -0,0 +1,28 @@
+fs = 48000;
+t = 0:1/fs:3-1/fs;
+f = 997;
+
+y = 32767 * sin(2*pi*f*t);
+
+Y = zeros(6,length(t));
+Y(1,:) = y;
+
+binwrite(Y,'../test_data/sine997_0dBFS.raw','short');
+
+%%
+
+rng(1);
+
+A = dbov_fac(y,-26);
+
+N = randn(size(y));
+AN = dbov_fac(N,-26);
+
+Y(1,:) = A * 32767 * sin(2*pi*997*t);
+Y(2,:) = 0.58 * A * 32767 * sin(2*pi*1033*t);
+Y(3,:) = 1.33 * A * 32767 * sin(2*pi*782*t);
+Y(4,:) = AN * randn(size(y));
+Y(5,:) = 0.45*AN * randn(size(y));
+Y(6,:) = 2.01*AN * randn(size(y));
+
+binwrite(Y,'../test_data/sine_noise_test.raw','short');
