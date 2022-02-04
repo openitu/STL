6c1660cd8f1051a4c198ca6f7095b2c1bd9f707e
ErikNorvell-Ericsson <erik.norvell@ericsson.com>
2022-02-04 16:04:49 +0100

    Updated test vectors and generator script

--- src/bs1770demo/supplementary_info/generate_test_signal.m
+++ src/bs1770demo/supplementary_info/generate_test_signal.m
@@ -7,7 +7,7 @@ y = 32767 * sin(2*pi*f*t);
 Y = zeros(6,length(t));
 Y(1,:) = y;
 
-binwrite(Y,'../test_data/sine997_0dBFS.raw','short');
+binwrite(Y,'../test_data/sine997_0dBFS.pcm','short');
 
 %%
 
@@ -25,4 +25,4 @@ Y(4,:) = AN * randn(size(y));
 Y(5,:) = 0.45*AN * randn(size(y));
 Y(6,:) = 2.01*AN * randn(size(y));
 
-binwrite(Y,'../test_data/sine_noise_test.raw','short');
+binwrite(Y,'../test_data/sine_noise_test.pcm','short');
