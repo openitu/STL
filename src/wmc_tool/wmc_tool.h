460e897988dc1d5a1b71e04d49d7cfa4b9f4901f
malenov <vladimir.malenovsky@usherbrooke.ca>
2025-05-16 10:35:06 +0200

    increase version number to v1.8

--- src/wmc_tool/wmc_tool.h
+++ src/wmc_tool/wmc_tool.h
@@ -19,7 +19,7 @@
  * Switches
  *-------------------------------------------------------------------*/
 
-#define WMC_TOOL_VERSION_NO  "1.7"          /* Current version */
+#define WMC_TOOL_VERSION_NO  "1.8"          /* Current version */
 /*#define DEBUG_PRINT*/                     /* For debugging purposes */
 
 /*-------------------------------------------------------------------*
